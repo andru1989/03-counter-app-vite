50e24f7f7021863ee13360c3fb3b72a340729741
Andres Vizcaino <andruvizcaino1989@gmail.com>
2023-04-24 18:50:38 -0500

    feat: add new tests cases to CounterApp

--- src/CounterApp.jsx
+++ src/CounterApp.jsx
@@ -6,7 +6,6 @@ export const CounterApp = ({ value }) => {
   const [ counter, setCounter ] = useState( value );
 
   const handleAdd = () => {
-    // console.log(event)
     setCounter( counter + 1 );
     // setCounter( (c) => c + 1 )
   };
--- tests/CounterApp.test.jsx
+++ tests/CounterApp.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import { CounterApp } from '../src/CounterApp';
 
 describe('Tests in <CounterApp />', () => {
@@ -12,6 +12,37 @@ describe('Tests in <CounterApp />', () => {
   test('should have the in initial value in 100', () => {
     render(<CounterApp value={initialValue} />);
     expect(screen.getByText(initialValue).innerHTML).toBeTruthy();
+  });
+
+  test('should increase number by 1 to initial value', () => {
+    render(<CounterApp value={initialValue} />);
+    const newValue = initialValue + 1;
+    const button = screen.getByText('+1');
+
+    fireEvent.click(button);
+    expect(screen.getByText(newValue)).toBeTruthy();
+  });
+
+  test('should decrease number by 1 to initial value', () => {
+    render(<CounterApp value={initialValue} />);
+    const newValue = initialValue - 1;
+    const button = screen.getByText('-1');
+
+    fireEvent.click(button);
+    expect(screen.getByText(newValue)).toBeTruthy();
+  });
+
+  test('should initialize value with reset button', () => {
+    render(<CounterApp value={initialValue} />);
+
+    const resetButton = screen.getByRole('button', { name: 'btn-reset' });
+    const decreaseButton = screen.getByText('-1');
+
+    fireEvent.click(decreaseButton);
+    fireEvent.click(decreaseButton);
+    fireEvent.click(resetButton);
+
+    expect(screen.getByText(initialValue)).toBeTruthy();
   });
 
 });
